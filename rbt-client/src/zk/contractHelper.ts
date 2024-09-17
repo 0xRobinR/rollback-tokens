d020c05a47f215c1dbc140e16fb7fd6bfd470d67
Robin <0xrobinrajput@gmail.com>
2024-09-17 23:20:16 +0530

    update file

---
+++ rbt-client/src/zk/contractHelper.ts
@@ -0,0 +1,12 @@
+// src/zk/contractHelper.ts
+import { ethers } from "ethers";
+
+declare global {
+    interface Window {
+      ethereum: any;
+  }
+}
+
+export async function submitProof(proof: any, publicSignals: any) {
+ 
+}
